86e8edf576e4805aa6406e01b37794b6b91ed520
Abdikhadir <Abdulkadir.muhiyadin@student.moringaschool.com>
2021-09-06 11:55:12 +0300

    created variable for days of ofthe week

--- js/scripts.js
+++ js/scripts.js
@@ -11,22 +11,4 @@ var submission = (e) => {
       "Friday",
       "Saturday"
     ];
-    var maleNames = [
-      "Kwasi",
-      "Kwadwo",
-      "Kwabena",
-      "Kwaku",
-      "Yaw",
-      "Kofi",
-      "Kwame"
-    ];
-    var femaleNames = [
-      "Akosua",
-       " Adwoa",
-      "Abenaa",
-      "Akua",
-      "Yaa",
-      "Afua",
-      "Ama"
-    ];
-}
+  }
