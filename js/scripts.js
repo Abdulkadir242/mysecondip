f44b9832f03a4ad88e7faaaa306aaa6cab2a379d
Abdikhadir <Abdulkadir.muhiyadin@student.moringaschool.com>
2021-09-06 12:09:38 +0300

    addd arrow function

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+
+var submission = (e) => {
+  e.preventDefault()
+  var frm = document.querySelector('Akan')
+    var daysOfTheWeek = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday"
+    ];
+  var maleNames = [
+    "Kwasi",
+    "Kwadwo",
+    "Kwabena",
+    "Kwaku",
+    "Yaw",
+    "Kofi",
+    "Kwame"
+    ];
+    var femaleNames = [
+    "Akosua",
+     " Adwoa",
+    "Abenaa",
+    "Akua",
+    "Yaa",
+    "Afua",
+    "Ama"
+    ];
+    }
